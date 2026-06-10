2d723a1482e354ee849ba439d8d70da853e47e95
Roshan Karki <roshankarki549@gmail.com>
2026-06-11 05:41:44 +0545

    latest commit

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 python manage.py migrate
 python manage.py collectstatic --noinput
 
-gunicorn library.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn library.library.wsgi:application --bind 0.0.0.0:$PORT
