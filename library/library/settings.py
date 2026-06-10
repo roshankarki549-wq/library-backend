a303cb3777bc858641f13e3ebd70d9428d6716ff
Roshan Karki <roshankarki549@gmail.com>
2026-06-11 04:30:59 +0545

    latest commit

--- library/library/settings.py
+++ library/library/settings.py
@@ -18,8 +18,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS",
-    "localhost,127.0.0.1").split(",")
+ALLOWED_HOSTS = ["*"]
 
 # ALLOWED_HOSTS = [
 #     "localhost",
