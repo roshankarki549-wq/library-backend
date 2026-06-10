c9f19d43b7fe0de78772190dca9bd5b19c6ce4c2
Roshan Karki <roshankarki549@gmail.com>
2026-06-11 02:39:55 +0545

    made changes in settings

--- library/library/settings.py
+++ library/library/settings.py
@@ -179,16 +179,10 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Email Configuration
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
 EMAIL_HOST = 'smtp.gmail.com'
-
 EMAIL_PORT = 587
-
 EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = 'roshankarki549@gmail.com'
-
-EMAIL_HOST_PASSWORD = 'jvpypirnzhcsfieo'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
