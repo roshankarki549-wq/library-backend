c6ce5301a0ab1f658e25acf5eac7ae26a025e658
Roshan Karki <roshankarki549@gmail.com>
2026-06-11 04:22:29 +0545

    latest commit

--- library/library/settings.py
+++ library/library/settings.py
@@ -166,6 +166,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
