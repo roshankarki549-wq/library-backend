be4cd5583c4babc5d4ba1cd57cd30c941e056a6d
Roshan Karki <roshankarki549@gmail.com>
2026-06-27 18:11:39 +0545

    updated settings

--- library/settings.py
+++ library/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = [".vercel.app","localhost","127.0.0.1"]
+ALLOWED_HOSTS = [".vercel.app","localhost","127.0.0.1","django-render-libraryapi-backend.onrender.com"]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://library-backend-railway-production.up.railway.app/",
