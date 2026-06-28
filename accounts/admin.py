5439248a41dc695c3d876b3810c1813420c7e81a
Roshan Karki <roshankarki549@gmail.com>
2026-06-28 12:34:13 +0545

    added student profile in admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User
+from .models import User, StudentProfile
 
 
 @admin.register(User)
@@ -16,4 +16,24 @@ class UserAdmin(admin.ModelAdmin):
     list_filter = (
         'role',
         'is_active'
+    )
+
+@admin.register(StudentProfile)
+class StudentProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'roll_no',
+        'department',
+        'kyc_status',
+    )
+
+    list_filter = (
+        'department',
+        'kyc_status',
+    )
+
+    search_fields = (
+        'user__username',
+        'roll_no',
+        'department',
     )
